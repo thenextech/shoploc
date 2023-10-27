2cc00a574c534885947a27bced7ec55fd18448c1
SAHED Anis <anis.sahed.etu@univ-lille.fr>
2023-10-27 18:28:53 +0200

    updating AdminRepositoryTest.java

--- src/test/java/nextech/shoploc/repositories/AdminRepositoryTest.java
+++ src/test/java/nextech/shoploc/repositories/AdminRepositoryTest.java
@@ -1,11 +1,9 @@
 package nextech.shoploc.repositories;
 
 import nextech.shoploc.domains.Admin;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
@@ -14,20 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AdminRepositoryTest {
-    /*
+
     @Autowired
     private AdminRepository adminRepository;
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+
     @Test
-    public void testFindByEmail() {
-        Optional<Admin> adminResponseDTO = adminRepository.findAdminByEmail("test@admin.com");
-        assertTrue(adminResponseDTO.isPresent());
-        assertEquals("test@admin.com", adminResponseDTO.get().getEmail());
-    }*/
+    public void testFindAdminByEmail() {
+        Admin admin = new Admin();
+        admin.setEmail("test@admin.com");
+        adminRepository.save(admin);
+
+        Optional<Admin> found = adminRepository.findAdminByEmail("test@admin.com");
+
+        assertTrue(found.isPresent());
+        assertEquals("test@admin.com", found.get().getEmail());
+    }
 
 }
